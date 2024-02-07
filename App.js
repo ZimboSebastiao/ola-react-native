0412942ca4a978764a0311357a4a73ad24f0b721
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-02-07 10:30:14 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const vai = () => {
+    Alert.alert("Titulo da janela", "Fala parça");
+    console.log("Teste");
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View>
-        <Text>Olá React Native</Text>
-      </View>
+      <SafeAreaView>
+        <View>
+          <Text>Cabeçalho</Text>
+        </View>
+
+        <View>
+          <Text>Conteúdo</Text>
+        </View>
+        <View>
+          <Text>Rodapé</Text>
+          <Button title="Toca aqui!" onPress={vai} />
+        </View>
+      </SafeAreaView>
     </>
   );
 }
